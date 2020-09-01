a29bb91743032efc20e4d182c65a91df13acd338
rokasandreikenas <shyptt@gmail.com>
2020-09-01 12:02:25 +0300

    Version 1.0.0

--- server.js
+++ server.js
@@ -21,10 +21,12 @@ const logger = require("./config/logger");
 const app = express();
 
 // React app 
+app.use(express.static("client/build"));
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, "client", 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
